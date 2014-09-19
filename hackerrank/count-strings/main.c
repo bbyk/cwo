d753e232063efa31a702c341e830599e4e7d46ac
Boris Byk <boris.byk@gmail.com>
2014-09-19 13:02:33 -0700

    implement NFA parser.

--- hackerrank/count-strings/main.c
+++ hackerrank/count-strings/main.c
@@ -3,121 +3,200 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 const unsigned PRIME = 1000000007;
+
+/* MEMORY POOL DATA/OPERATIONS */
+
+const unsigned MP_CAPACITY = 60 * 1024 * 1024; // 60 MB
+const unsigned MAX_PATTERN_LENGTH = 100;
+
+typedef struct __mpool {
+    void *head;
+    void *cur;
+    void *cap;
+} mpool;
+
+mpool *mp_new(size_t mp_capacity) {
+    mpool *p = malloc(sizeof(mpool));
+    p->head = malloc(mp_capacity);
+    p->cap = p->head + mp_capacity;
+    p->cur = p->head;
+    return p;
+}
+
+void *mp_alloc(mpool *p, size_t size) {
+    if (p->cur + size > p->cap)
+        return NULL;
+    void *r = p->cur;
+    p->cur += size;
+    return r;
+}
+
+void mp_reset(mpool *p) {
+    p->cur = p->head;
+}
+
+void mp_delete(mpool *p) {
+    free(p->head);
+    free(p);
+}
+
+mpool *pool;
+
+/* END MEMORY POOL DATA/OPERATIONS */
 
 unsigned min(unsigned a, unsigned b) {
     return a < b ? a : b;
 }
 
-typedef struct __node node;
+typedef struct __transition {
+    char c;
+    unsigned sid;
+} transition;
+
+typedef struct __nfa nfa;
+
+typedef struct __state {
+    unsigned sid;
+    transition trans[3];
+    unsigned t_id;
+} state;
 
 // a delegate we use for polymorphism
-typedef unsigned count_cb(node *, unsigned l);
+typedef unsigned count_cb(nfa *, unsigned l);
 
-struct __node {
-    node *l, *r;
-    count_cb *call;
-    unsigned min_size;
-    bool fix_size;
+struct __nfa {
+    unsigned l_sid;
+    unsigned r_sid;
+    state states[MAX_PATTERN_LENGTH * 10];
+    unsigned st_id;
 };
 
-node *parse_node(char const **caret);
+typedef struct __parser_ctx {
+    nfa *pnfa;
+    unsigned sid_stack[MAX_PATTERN_LENGTH];
+    unsigned stack_id;
+    char const **caret;
+} parser_ctx;
 
-unsigned one_letter_re(node *np, const unsigned l) {
-    return l > 1 ? 0 : 1;
+unsigned newState(nfa *pnfa) {
+    pnfa->states[pnfa->st_id].t_id = 0;
+    pnfa->states[pnfa->st_id].sid = pnfa->st_id;
+    return pnfa->st_id++;
 }
 
-unsigned or_re(node *np /* node pointer */, const unsigned l) {
-    unsigned long sum = 0;
-    sum = (sum + np->l->call(np->l, l)) % PRIME;
-    sum = (sum + np->r->call(np->l, l)) % PRIME;
-    return (unsigned) sum;
-}
+void parse_node(parser_ctx *);
 
-unsigned and_re(node *np, const unsigned l) {
-    unsigned min_size = np->l->min_size + np->r->min_size;
-
-    if (min_size > l)
-        return 0;
-    else if (l == min_size) {
-        return 1;
-    } else if (np->l->fix_size && np->r->fix_size) {
-        return 0;
-    } else {
-        unsigned long sum = 0;
-        sum = (sum + (np->r->min_size ? l / np->r->min_size : 0) * np->l->call(np->l, l - np->r->min_size)) % PRIME;
-        sum = (sum + (np->l->min_size ? l / np->l->min_size : 0) * np->r->call(np->r, l - np->l->min_size)) % PRIME;
-        return (unsigned) sum;
-    }
-}
-
-unsigned multi_re(node *np, const unsigned l) {
-    return np->l->min_size == 1 ? 1 : np->l->min_size * l;
-}
-
-node *parse(const char *line, unsigned const *l) {
+nfa *regex_to_nfa(const char *line, unsigned *l) {
     const char *c = line;
     while (*c != ' ')
         c++;
     sscanf(c, "%u", l);
 
-    return parse_node(&line);
-}
+    parser_ctx *pctx = mp_alloc(pool, sizeof(parser_ctx));
+    pctx->caret = &line;
+    pctx->stack_id = 0;
+    pctx->pnfa = mp_alloc(pool, sizeof(nfa));
+    pctx->pnfa->st_id = 0;
 
-/**
-* post-order traversal to free the memory.
-*/
-void delete_node(node *pn) {
-    if (pn->l)
-        delete_node(pn->l);
-    if (pn->r)
-        delete_node(pn->r);
-    free(pn);
+    parse_node(pctx);
+
+    // pop last two items from the stack as initial and acceptance state ids.
+    pctx->pnfa->r_sid = pctx->sid_stack[--pctx->stack_id];
+    pctx->pnfa->l_sid = pctx->sid_stack[--pctx->stack_id];
+
+    return pctx->pnfa;
 }
 
 /**
 * Uses reader pattern - a token is read moves caret after the token.
+* http://en.wikipedia.org/wiki/Thompson's_construction_algorithm
 */
-node *parse_node(char const **caret) {
-    node *rt = NULL;
-    char c = **caret;
+void parse_node(parser_ctx *pctx) {
+    char c = **pctx->caret;
+    unsigned l, r;
+    state *lstate, *pop_state;
 
     if ('a' == c || 'b' == c) {
-        rt = malloc(sizeof(node));
-        rt->call = &one_letter_re;
-        rt->min_size = 1;
-        rt->r = rt->l = NULL;
-        rt->fix_size = true;
-        (*caret)++;
+        l = newState(pctx->pnfa);
+        r = newState(pctx->pnfa);
+        lstate = &pctx->pnfa->states[l];
+        lstate->trans[lstate->t_id].c = c;
+        lstate->trans[lstate->t_id++].sid = r;
+        (*pctx->caret)++;
     } else if ('(' == c) {
-        rt = malloc(sizeof(node));
-        (*caret)++; // move beyond '('
-        rt->l = parse_node(caret);
-        c = **caret;
+        (*pctx->caret)++; // move beyond '('
+        parse_node(pctx);
+        c = **pctx->caret;
         if ('|' == c) {
-            rt->call = &or_re;
-            (*caret)++;
-            rt->r = parse_node(caret);
-            rt->min_size = min(rt->l->min_size, rt->r->min_size);
-            rt->fix_size = rt->l->fix_size & rt->r->fix_size;
+            (*pctx->caret)++;
+            parse_node(pctx);
+
+            // connect the left state to the popped state on the stack.
+            l = newState(pctx->pnfa);
+            lstate = &pctx->pnfa->states[l];
+            lstate->trans[lstate->t_id].c = 'e';
+            lstate->trans[lstate->t_id++].sid = pctx->sid_stack[--pctx->stack_id];
+
+            // connect the popped state to the right state
+            r = newState(pctx->pnfa);
+            pop_state = &pctx->pnfa->states[pctx->sid_stack[--pctx->stack_id]];
+            pop_state->trans[pop_state->t_id].c = 'e';
+            pop_state->trans[pop_state->t_id++].sid = r;
+
+            // connect the left state to the popped state on the stack.
+            lstate->trans[lstate->t_id].c = 'e';
+            lstate->trans[lstate->t_id++].sid = pctx->sid_stack[--pctx->stack_id];
+
+            // connect the popped state to the right state
+            pop_state = &pctx->pnfa->states[pctx->sid_stack[--pctx->stack_id]];
+            pop_state->trans[pop_state->t_id].c = 'e';
+            pop_state->trans[pop_state->t_id++].sid = r;
         } else if ('*' == c) {
-            rt->call = &multi_re;
-            rt->min_size = 0;
-            rt->fix_size = false;
-            (*caret)++;
-            rt->r = NULL;
+            (*pctx->caret)++;
+            l = newState(pctx->pnfa);
+            lstate = &pctx->pnfa->states[l];
+            lstate->trans[lstate->t_id].c = 'e';
+            unsigned pop_r = pctx->sid_stack[--pctx->stack_id];
+            lstate->trans[lstate->t_id++].sid = pop_r;
+
+            r = newState(pctx->pnfa);
+
+            lstate->trans[lstate->t_id].c = 'e';
+            lstate->trans[lstate->t_id++].sid = r;
+
+            pop_state = &pctx->pnfa->states[pctx->sid_stack[--pctx->stack_id]];
+            pop_state->trans[pop_state->t_id].c = 'e';
+            pop_state->trans[pop_state->t_id++].sid = r;
+
+            pop_state->trans[pop_state->t_id].c = 'e';
+            pop_state->trans[pop_state->t_id++].sid = pop_r;
         } else {
-            rt->call = &and_re;
-            rt->r = parse_node(caret);
-            rt->min_size = rt->l->min_size + rt->r->min_size;
-            rt->fix_size = rt->l->fix_size & rt->r->fix_size;
+            parse_node(pctx);
+
+            // pop left and right state of first pair on the stack
+            l = pctx->sid_stack[--pctx->stack_id];
+            pop_state = &pctx->pnfa->states[pctx->sid_stack[--pctx->stack_id]];
+
+            // pop left state of the second pair on the stack and connect with an epsilon
+            r = pctx->sid_stack[--pctx->stack_id];
+            pop_state->trans[pop_state->t_id].c = 'e';
+            pop_state->trans[pop_state->t_id++].sid = r;
+
+            // pop real right state of the second pair.
+            r = pctx->sid_stack[--pctx->stack_id];
         }
 
         // for the closing ')'
-        (*caret)++;
+        (*pctx->caret)++;
     }
-    return rt;
+
+    // put new l and r back on the stack
+    pctx->sid_stack[pctx->stack_id++] = l;
+
+    pctx->sid_stack[pctx->stack_id++] = r;
 }
 
 int main(int argc, const char *argv[]) {
@@ -135,15 +214,19 @@ int main(int argc, const char *argv[]) {
     getline(&line, &len, fp);
     sscanf(line, "%u", &T);
 
+    pool = mp_new(MP_CAPACITY);
+
     while (T--) {
         getline(&line, &len, fp);
         unsigned L;
-        node *pn = parse(line, &L);
+        nfa *pn = regex_to_nfa(line, &L);
 
-        printf("%u\n", pn->call(pn, L));
+//        printf("%u\n", pn->call(pn, L));
 
-        delete_node(pn);
+        mp_reset(pool);
     }
+
+    mp_delete(pool);
 
     free(line);
     if (fp != stdin)
