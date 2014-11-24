30a4e6aabf5145de87e4a9f995fc535e40de24cb
Boris Byk <boris.byk@gmail.com>
2014-11-23 19:36:32 -0800

    Working version of dijkstra with priority queue.

--- python/coursera/algo-006/dijkstra.py
+++ python/coursera/algo-006/dijkstra.py
@@ -2,6 +2,13 @@ import sys
 
 __author__ = 'bbyk'
 
+"""
+Dijkstra algorithm with priority queue. Supported operations:
+1. insert with priority
+2. extract min
+3. decrease priority 
+"""
+
 
 class PriorityQueue:
     def __init__(self):
@@ -11,7 +18,13 @@ class PriorityQueue:
     def __contains__(self, item):
         return item in self.fast_line
 
+    def assert_link_inv(self):
+        for k, v in self.fast_line.items():
+            assert k == self.heap[v][0]
+
     def insert(self, elem, priority, i=None):
+        # self.assert_link_inv()
+
         if i is None:
             self.heap.append([elem, priority])
             i = len(self.heap) - 1
@@ -23,25 +36,40 @@ class PriorityQueue:
             if self.heap[pi][1] > self.heap[i][1]:
                 self.heap[pi], self.heap[i] = self.heap[i], self.heap[pi]
                 self.fast_line[self.heap[i][0]] = i
+            else:
+                break
             i = pi
 
         self.fast_line[elem] = i
+        # self.assert_link_inv()
 
-    def sift_down(self, i):
+    def sift_down(self, i, l=None):
+        if l is None:
+            l = len(self.heap)
+        if not l:
+            return
+
         while True:
             ci = (i << 1) + 1
-            if ci >= len(self.heap):
+            if ci >= l:
                 break
 
-            if ci + 1 < len(self.heap) and self.heap[ci][1] > self.heap[ci + 1][1]:
+            if ci + 1 < l and self.heap[ci][1] > self.heap[ci + 1][1]:
                 ci += 1
 
-            self.heap[ci], self.heap[i] = self.heap[i], self.heap[ci]
-            self.fast_line[self.heap[i][0]] = i
+            if self.heap[i][1] > self.heap[ci][1]:
+                self.heap[ci], self.heap[i] = self.heap[i], self.heap[ci]
+                self.fast_line[self.heap[i][0]] = i
+            else:
+                break
 
             i = ci
 
+        self.fast_line[self.heap[i][0]] = i
+
     def extract_min(self):
+        # self.assert_link_inv()
+
         min = self.heap[0]
         del self.fast_line[min[0]]
 
@@ -52,6 +80,8 @@ class PriorityQueue:
         del self.heap[-1]
         self.sift_down(0)
 
+        # self.assert_link_inv()
+
         return min
 
     def __len__(self):
@@ -59,21 +89,26 @@ class PriorityQueue:
 
     def change_priority_if_higher(self, elem, priority):
         e_ix = self.fast_line[elem]
+
+        assert self.heap[e_ix][0] == elem
+
         if self.heap[e_ix][1] > priority:
             del self.fast_line[elem]
             last_i = e_ix
             while True:
-                ci = (last_i << 1) + 1
+                ci = (last_i << 1) + 2
                 if ci >= len(self.heap):
-                    break
-                if ci + 1 < len(self.heap) and self.heap[ci][1] > self.heap[ci + 1][1]:
-                    ci += 1
+                    ci -= 1
+                    if ci >= len(self.heap):
+                        break
                 last_i = ci
 
             if last_i != e_ix:
-                self.heap[e_ix], self.heap[last_i] = self.heap[e_ix], self.heap[last_i]
-                self.fast_line[self.heap[e_ix][0]] = e_ix
-                self.sift_down(e_ix)
+                self.heap[e_ix], self.heap[last_i] = self.heap[last_i], self.heap[e_ix]
+                self.heap[last_i] = None
+                # self.fast_line[self.heap[e_ix][0]] = e_ix
+                self.sift_down(e_ix, last_i)
+                # self.assert_link()
 
             self.insert(elem=elem, priority=priority, i=last_i)
 
@@ -92,7 +127,7 @@ class Dijkstra:
 
     def path_size_for(self, *v_ids):
         for v in v_ids:
-            yield str(self.VL[v - 1])
+            yield self.VL[v - 1]
 
     def calc_shortest(self):
         while self.queue:
@@ -102,7 +137,7 @@ class Dijkstra:
 
             for arc in self.V[next_v]:
                 if arc[0] in self.visited:
-                    pass
+                    continue
 
                 if arc[0] in self.queue:
                     self.queue.change_priority_if_higher(elem=arc[0], priority=length + arc[1])
@@ -129,4 +164,4 @@ if __name__ == "__main__":
         V[int(parts[0]) - 1] = tuple(
             [[int(t) - 1 if i == 0 else int(t) for i, t in enumerate(p.split(','))] for p in parts[1:]])
 
-    print(",".join(Dijkstra(graph=V, start=1).path_size_for(1, 2, 3, 4)))
+    print(",".join([str(t) for t in Dijkstra(graph=V, start=1).path_size_for(7, 37, 59, 82, 99, 115, 133, 165, 188, 197)]))
