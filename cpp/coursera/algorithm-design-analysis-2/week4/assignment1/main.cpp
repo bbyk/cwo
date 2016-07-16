e8cb0ec3a27c6d7771a6b2022a055cc8e959238c
Boris Byk <boris.byk@gmail.com>
2016-07-16 13:19:02 -0700

    Floyd-Warshall algorithm

--- cpp/coursera/algorithm-design-analysis-2/week4/assignment1/main.cpp
+++ cpp/coursera/algorithm-design-analysis-2/week4/assignment1/main.cpp
@@ -1,71 +1,87 @@
-// https://leetcode.com/problems/3sum/
+//Floyd-Warshall
 
 #include <fstream>
 #include <iostream>
 #include <cassert>
 #include <algorithm>
 #include <vector>
+#include <climits>
 #include <ctime>
 #include <map>
 #include <unordered_set>
 #include <unordered_map>
 
 using namespace std;
+using slong = long long;
 
 struct Edge {
-    int n1, n2;
-    unsigned d;
+    unsigned n1, n2;
+    slong d;
 };
 
 class Solution {
 public:
-    unsigned max_spacing(size_t k /* num of clusters */, vector<Edge> &edges) {
-        unordered_map<int, unordered_set<int> *> nu;
-
-        sort(edges.begin(), edges.end(), [](Edge &e1, Edge &e2) {
-            return e1.d < e2.d;
-        });
-        size_t numk = 0;
-        for (Edge &e : edges) {
-            auto &pset1 = nu[e.n1];
-            if (pset1 == nullptr) {
-                pset1 = new unordered_set<int>{e.n1};
-                ++numk;
-            }
-            auto &pset2 = nu[e.n2];
-            if (pset2 == nullptr) {
-                pset2 = new unordered_set<int>{e.n2};
-                ++numk;
+    bool shortest_shortest_path(size_t num_of_vertices, vector<Edge> &edges, slong& result) {
+        // run Floyd-Warshall, create a 3-D array
+        size_t d2_slice_size = num_of_vertices * num_of_vertices;
+        size_t d3_size = d2_slice_size * (num_of_vertices + 1);
+        slong *buffer = new slong[d3_size];
+        {
+            slong *b = buffer;
+            slong *e = buffer + d3_size;
+            while (b != e) {
+                *b++ = LLONG_MAX; // plus infinity
             }
         }
-        assert(numk >= k);
-        auto e_it = edges.begin();
-        while (e_it != edges.end()) {
-            Edge &e = *e_it;
-            // the nodes are in the same cluster, skip
-            auto cluster1 = nu[e.n1];
-            auto cluster2 = nu[e.n2];
-            // we're in the same cluster, skip
-            if (cluster1 != cluster2) {
-                if (numk == k) {
-                    break;
-                }
-                // merge to the set with the greater cardinality: cluster1 is greater set
-                if (cluster1->size() < cluster2->size()) {
-                    swap(cluster1, cluster2);
-                }
 
-                for (int node : *cluster2) {
-                    cluster1->insert(node);
-                    nu[node] = cluster1;
-                }
+        slong ***d3a = new slong **[num_of_vertices + 1];
 
-                delete cluster2;
-                --numk;
+        for (auto i = 0; i <= num_of_vertices; ++i) {
+            slong **slice = new slong *[num_of_vertices];
+            for (auto j = 0; j < num_of_vertices; ++j) {
+                slice[j] = buffer + d2_slice_size * i + num_of_vertices * j;
             }
-            ++e_it;
+            d3a[i] = slice;
         }
-        return e_it->d;
+
+        for (auto &edge : edges) {
+            d3a[0][edge.n1][edge.n2] = edge.d;
+            d3a[0][edge.n1][edge.n1] = 0LL;
+            d3a[0][edge.n2][edge.n2] = 0LL;
+        }
+
+        for (auto k = 1; k <= num_of_vertices; ++k) {
+            for (auto i = 0; i < num_of_vertices; ++i) {
+                for (auto j = 0; j < num_of_vertices; ++j) {
+                    d3a[k][i][j] = min(
+                            d3a[k - 1][i][j],
+                            d3a[k - 1][i][k - 1] == LLONG_MAX || d3a[k - 1][k - 1][j] == LLONG_MAX ? LLONG_MAX : d3a[k - 1][i][k - 1] + d3a[k - 1][k - 1][j]
+                    );
+                }
+            }
+        }
+
+        for (auto i = 0; i < num_of_vertices; ++i) {
+            slong v = d3a[num_of_vertices][i][i];
+            if (v < 0) {
+                return false;
+            }
+        }
+
+        result = d3a[num_of_vertices][0][1];
+        for (auto i = 0; i < num_of_vertices - 1; ++i) {
+            for (auto j = i + 1; j < num_of_vertices; ++j) {
+                result = min(result, d3a[num_of_vertices][i][j]);
+            }
+        }
+
+        // memory clean up
+        for (auto i = 0; i <= num_of_vertices; ++i) {
+            delete[] d3a[i];
+        }
+        delete[] buffer;
+
+        return true;
     }
 };
 
@@ -83,16 +99,26 @@ int main(int argc, char *argv[]) {
     }
     Edge e;
     vector<Edge> edges;
-    unsigned num_of_edges;
+    size_t num_of_vertices, num_of_edges;
+    *file >> num_of_vertices;
     *file >> num_of_edges;
     while (num_of_edges--) {
         *file >> e.n1;
         *file >> e.n2;
         *file >> e.d;
+        // let's deal with zero based indexing
+        --e.n1;
+        --e.n2;
         edges.push_back(e);
     }
     Solution solution;
 
-    cout << solution.max_spacing(4, edges) << endl;
+    slong result;
+    bool b = solution.shortest_shortest_path(num_of_vertices, edges, result);
+    if (b) {
+        cout << result << endl;
+    } else {
+        cout << "negative cycle" << endl;
+    }
     return 0;
 }
